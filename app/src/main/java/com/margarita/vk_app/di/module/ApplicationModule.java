622b0ddcc0017a4ec9deca5b83175b84000b6b37
Margarita <margo.himera@yandex.ru>
2017-11-05 22:49:42 +0300

    Layouts for news were added

--- app/src/main/java/com/margarita/vk_app/di/module/ApplicationModule.java
+++ app/src/main/java/com/margarita/vk_app/di/module/ApplicationModule.java
@@ -2,6 +2,7 @@ package com.margarita.vk_app.di.module;
 
 import android.app.Application;
 import android.content.Context;
+import android.graphics.Typeface;
 
 import javax.inject.Singleton;
 
@@ -21,5 +22,13 @@ public class ApplicationModule {
     @Singleton
     public Context provideContext() {
         return application;
+    }
+
+
+    @Provides
+    @Singleton
+    Typeface provideGoogleTypeface(Context context) {
+        return Typeface.createFromAsset(context.getAssets(),
+                "MaterialIcons-Regular.ttf");
     }
 }
