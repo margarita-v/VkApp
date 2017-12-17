8f1a409f68572962b014e4408ad59eca18d00170
Margarita <margo.himera@yandex.ru>
2017-12-17 14:04:01 +0300

    All layouts for attachments were added

--- app/src/main/java/com/margarita/vk_app/models/LayoutTypes.java
+++ app/src/main/java/com/margarita/vk_app/models/LayoutTypes.java
@@ -11,11 +11,25 @@ public enum LayoutTypes {
     NewsFeedItemHeader(R.layout.item_news_header),
     NewsFeedItemBody(R.layout.item_news_body),
     NewsFeedItemFooter(R.layout.item_news_footer),
+
     Member(R.layout.member_item),
     Topic(R.layout.topic_item),
+
     InfoStatus(R.layout.item_info_status),
     InfoContacts(R.layout.item_info_contacts),
-    InfoLinks(R.layout.item_info_links);
+    InfoLinks(R.layout.item_info_links),
+
+    AttachmentAudio(R.layout.item_attachment_audio),
+    AttachmentDoc(R.layout.item_attachment_doc),
+    AttachmentDocImage(R.layout.item_attachment_doc_image),
+    AttachmentImage(R.layout.item_attachment_image),
+    AttachmentLink(R.layout.item_attachment_link),
+    AttachmentLinkExternal(R.layout.item_attachment_link_external),
+    AttachmentPage(R.layout.item_attachment_page),
+    AttachmentVideo(R.layout.item_attachment_video),
+
+    OpenedPostHeader(R.layout.item_opened_post_header),
+    OpenedPostRepostHeader(R.layout.item_opened_post_repost_header);
 
     private final int id;
 
