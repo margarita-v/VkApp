ac6b843487a0073a5f8001d6b1aaee13a726a773
Margarita <margo.himera@yandex.ru>
2017-12-20 22:37:38 +0300

    UIHelper was added

---
+++ app/src/main/java/com/margarita/vk_app/common/utils/UIHelper.java
@@ -0,0 +1,28 @@
+package com.margarita.vk_app.common.utils;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.view.View;
+import android.widget.TextView;
+
+public class UIHelper {
+
+    private UIHelper ourInstance;
+
+    private Resources resources;
+    private Context context;
+
+    public UIHelper getInstance() {
+        return ourInstance != null ? ourInstance : new UIHelper();
+    }
+
+    /**
+     * Set up text view with its text and visibility
+     * @param textView TextView widget
+     * @param text Text for TextView
+     */
+    public void setUpTextView(TextView textView, String text) {
+        textView.setText(text);
+        textView.setVisibility(text.isEmpty() ? View.GONE : View.VISIBLE);
+    }
+}
