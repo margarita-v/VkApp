07cdc4ccb0b881fddad25f5c080f66d640e9679c
Margarita <margo.himera@yandex.ru>
2017-12-21 23:56:23 +0300

    Layouts for comments were added

--- app/src/main/java/com/margarita/vk_app/common/utils/UIHelper.java
+++ app/src/main/java/com/margarita/vk_app/common/utils/UIHelper.java
@@ -5,6 +5,8 @@ import android.content.res.Resources;
 import android.view.View;
 import android.widget.TextView;
 
+import com.margarita.vk_app.R;
+
 //TODO Remove this class
 public class UIHelper {
 
@@ -12,6 +14,11 @@ public class UIHelper {
 
     private Resources resources;
     private Context context;
+
+    /**
+     * Message for reposts
+     */
+    private static final String REPOST_MESSAGE = "Поделился";
 
     public UIHelper getInstance() {
         return ourInstance != null ? ourInstance : new UIHelper();
@@ -25,5 +32,23 @@ public class UIHelper {
     public static void setUpTextView(TextView textView, String text) {
         textView.setText(text);
         textView.setVisibility(text.isEmpty() ? View.GONE : View.VISIBLE);
+    }
+
+    /**
+     * Set up text view for display the message for reposts
+     * @param textView TextView widget
+     * @param message Repost message
+     */
+    public void setUpReposts(TextView textView, String message) {
+        int color;
+        if (!message.isEmpty()) {
+            textView.setVisibility(View.VISIBLE);
+            textView.setText(message);
+            color = android.R.color.primary_text_light;
+        } else {
+            textView.setText(REPOST_MESSAGE);
+            color = R.color.colorIcon;
+        }
+        textView.setTextColor(textView.getResources().getColor(color));
     }
 }
